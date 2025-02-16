3279b41465b3d4c98e0e94367310e0b0fe45e35d
malitha04 <115771388+malitha04@users.noreply.github.com>
2025-02-16 20:20:52 +0530

    Add files via upload

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,35 +1,21 @@
-import java.util.Arrays;
-public class BubbleSort{
+public class BubbleSort {
 
-   public void swap(int[]arr,int i,int j){
-      
-	  int temp = arr[i];
-	  arr[i] = arr[j];
-	  arr[j] = temp;
-	  }
-	  
-	  public void bubbleSort(int[]array){
-	  int n =array.length;
-	  for(int i = 0;i<n-1;i++){
-	  for(int j = 0;j<n-1-i;j++){
-	  if(array[j]>array[j+1]){
-	  
-	  swap(array,j,j+1);
-	  }
-     }
-	}
-   }
-    public static void main(String[]args)
-	{
-	int[] numbers ={8,3,1,4,5,11,7,2,17,13};
-	
-	System.out.println("Before Sorting:" + Arrays.toString(numbers));
-	
-	BubbleSort sorter = new BubbleSort();
-	
-	sorter.bubbleSort(numbers);
-	
-	System.out.println("After Sorting:" + Arrays.toString(numbers));
-	  }
-	  }
-	  
+    // Method to swap two elements in an array
+    public void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    // Method to perform Bubble Sort on the array
+    public void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr, j, j + 1);
+                }
+            }
+        }
+    }
+}
